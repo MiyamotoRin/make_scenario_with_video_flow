3581962217143f4745a3cf7c7dba771bbe8bdd1c
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-11-30 03:08:58 +0900

    test: 動画のパスを取得できるようになる

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from scene_detect import search_video
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": "*"}})
 
-@app.route('/path-to-your-python-endpoint', methods=['POST'])
+@app.route('/', methods=['POST'])
 def process_video_path():
     data = request.json
-    video_path = data['videoPath']
+    scene_description= data['sceneDescription']
+    print(scene_description)
     # ここで動画パスを処理するロジックを実装
-
+    video_id = search_video.get_video_index([scene_description], 'scene_detect/index.usearch')
+    video_path = search_video.get_video_path(video_id, 'scene_detect/file_paths.db')
+    print(video_path)
     # 処理結果をJSONとして返す
-    return jsonify(videoPath=video_path)
+    return jsonify(sceneDescription=video_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scripts/send_back.js
+++ scripts/send_back.js
@@ -1,20 +1,23 @@
-document.getElementById('submitVideoPath').addEventListener('click', function() {
-    var videoPath = document.getElementById('videoPathInput').value;
+document.getElementById('submitSceneDescription').addEventListener('click', function(event) {
+    event.preventDefault();
+    var sceneDescription = document.getElementById('sceneDescriptionInput').value;
+    console.log(sceneDescription)
   
     // ここでAJAXリクエストを使用してPythonプログラムにデータを送信
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/path-to-your-python-endpoint", true);
+    xhr.open("POST", "http://127.0.0.1:5000/", true);
     xhr.setRequestHeader("Content-Type", "application/json");
   
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
         // レスポンスとして動画のパスを受け取る
         var response = JSON.parse(xhr.responseText);
-        document.getElementById('videoPathResult').innerText = response.videoPath;
-      }
+        console.log(response);
+        document.getElementById('sceneDescriptionResult').innerText = response.sceneDescription;
+        }
     };
   
     // JSONとしてデータを送信
-    xhr.send(JSON.stringify({videoPath: videoPath}));
-  });
+    xhr.send(JSON.stringify({sceneDescription: sceneDescription}));
+});
   
