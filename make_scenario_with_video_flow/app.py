65e5902697118d7267f160228904db963e08ed7b
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2024-01-23 13:53:57 +0900

    chore: ディレクトリ移動

--- make_scenario_with_video_flow/app.py
+++ make_scenario_with_video_flow/app.py
@@ -11,11 +11,11 @@ def process_video_path():
     scene_description= data['sceneDescription']
     print(scene_description)
     # ここで動画パスを処理するロジックを実装
-    video_id = search_video.get_video_index([scene_description], 'scene_detect/index.usearch')
-    video_path = search_video.get_video_path(video_id, 'scene_detect/file_paths.db')
-    print(video_path)
+    base_video_id = search_video.get_video_index([scene_description], 'scene_detect/index.usearch')
+    base_video_path = search_video.get_video_path(base_video_id, 'scene_detect/file_paths.db')
+    
     # 処理結果をJSONとして返す
-    return jsonify(sceneDescription=video_path)
+    return jsonify(sceneDescription=base_video_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
